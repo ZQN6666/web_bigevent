935dfa47e56500e6fdb93b4d1ee1626af446bae0
nana <772953351@qq.com>
2022-10-23 11:51:02 +0800

    完成部分文章管理功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,6 +6,7 @@ $.ajaxPrefilter(function(options){
     options.url='http://www.liulongbin.top:3007'+options.url
     // console.log(options.url);
     // 统一为有权限的接口设置headers请求头
+    
     if(options.url.indexOf('/my')!==-1){
         options.headers={
         Authorization:localStorage.getItem('token')||''
